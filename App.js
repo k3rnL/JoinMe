bb7e86e8a841180744fbae5bc4b9b0e8047f2046
Daniel Erwan <daniel.erwan31@gmail.com>
2020-01-05 10:52:27 +0100

    Fix map

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView from "react-native-maps";
 
 export default function App() {
