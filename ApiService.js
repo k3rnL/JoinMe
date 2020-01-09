cfdc01158a1e3284f2bc6c4fb8f975f71dd170d0
Daniel Erwan <daniel.erwan31@gmail.com>
2020-01-09 21:31:52 +0100

    Add api service

---
+++ ApiService.js
@@ -0,0 +1,41 @@
+
+
+const API_URL = 'https://join-me-api.herokuapp.com';
+
+class Party {
+    name;
+    id;
+    address;
+    members;
+}
+
+export class ApiService {
+
+    static async getUserParties(uid) {
+        let response = await fetch(API_URL + '/users/' + uid + '/parties',
+            {
+                method: 'GET',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                }
+            });
+        let parties = await response.json();
+        return parties;
+        // return Object.assign([], JSON.parse(response.data).parties);
+    }
+
+    static async registerUser(uid, fcmToken) {
+        let response = await fetch(API_URL + '/users/register',
+            {
+                method: 'POST',
+                headers: {
+                    Accept: 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({uid, fcm_token: fcmToken})
+            });
+        await response.json();
+    }
+
+}
