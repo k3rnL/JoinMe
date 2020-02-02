05093d62370fb674a5aca03ab4801a935306e8db
Daniel Erwan <daniel.erwan31@gmail.com>
2020-02-02 22:30:32 +0100

    Connect Home to redux store

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import MapScreen from './../components/Map';
 import ContactsScreen from '../components/ContactsList';
 import {Button} from "react-native";
+import {connect} from 'react-redux'
+import PropTypes from 'prop-types'
 
 class Home extends Component {
     render() {
@@ -11,6 +13,7 @@ class Home extends Component {
                 <View style={styles.header}>
                     <Button title={'Contacts'} onPress={() => this.props.navigation.navigate('ContactSelection')}/>
                     <Button title={'Auth'} onPress={() => this.props.navigation.navigate('Auth')}/>
+                    <Text>{this.props.uid}</Text>
                 </View>
                 <View style={styles.content}>
                     <MapScreen />
@@ -47,4 +50,8 @@ const styles = StyleSheet.create({
     },
 });
 
-export default Home;
+export default connect(state => {
+    console.log(state);
+    return {uid: state.profile.uid };
+})(Home);
+// export default connect(state => ({ uid: state.uid }))(Home);
