b67704d8b34c69ca2bd89e08154561aa5a6b4433
Daniel Erwan <daniel.erwan31@gmail.com>
2020-02-02 22:29:31 +0100

    Add auth loading page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createSwitchNavigator } from 'react-navigation';
 import { registerRootComponent } from 'expo';
 import Home from './pages/HomePage';
 import Auth from './pages/AuthPage'
 import ContactSelection from "./pages/ContactSelectionPage";
 import firebaseConfig from "../config/firebase";
 import * as firebase from "firebase";
+import {connect, Provider} from "react-redux";
+import React from "react";
+import store from './stores/index'
+import AuthLoading from "./pages/AuthLoadingPage";
+import {ApiService} from "./services/ApiService";
+import {updateUid} from "./action/profile";
 
 firebase.initializeApp(firebaseConfig);
 
@@ -23,5 +30,29 @@ const HomeStack = createStackNavigator({
     initialRouteName: 'Home'
 });
 
-registerRootComponent(createAppContainer(HomeStack));
+const AuthStack = createSwitchNavigator(
+    {
+        AuthLoading: AuthLoading,
+        App: HomeStack,
+        Auth: Auth,
+    },
+    {
+        initialRouteName: 'AuthLoading',
+    }
+);
+
+const AppContainer = createAppContainer(AuthStack);
+
+class App extends React.Component {
+
+    render() {
+        return (
+            <Provider store={store}>
+                <AppContainer />
+            </Provider>
+        );
+    }
+}
+
+registerRootComponent(App);
 
---
+++ src/pages/AuthLoadingPage.jsx
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+import * as firebase from "firebase";
+import {updateUid} from "../action/profile";
+import {connect, Provider} from "react-redux";
+
+
+class AuthLoadingPage extends Component {
+
+    componentDidMount() {
+        this.listener = firebase.auth().onAuthStateChanged(
+            async authUser => {
+                let uid;
+                if (authUser === null) {
+                    uid = null;
+                    this.props.dispatch(updateUid(uid));
+                    this.props.navigation.navigate('Auth');
+                }
+                else {
+                    uid = authUser.uid;
+                    this.props.dispatch(updateUid(uid));
+                    this.props.navigation.navigate('App');
+                }
+            },
+            () => {
+                // this.props.dispatch({type: 'TEST', null});
+            },
+        );
+    }
+
+    render() {
+        return (
+            <View/>
+        );
+    }
+
+}
+
+export default connect(state => ({ uid: state.uid }))(AuthLoadingPage);
