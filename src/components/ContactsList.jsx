5488778d8aba238e6e2ba2666944d6231a3b97bf
Ludovic_Mandagot <ludo@demooz.com>
2020-02-24 22:40:02 +0100

    redo contactlist

--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -1,127 +1,135 @@
-import React, {useState, useEffect} from 'react';
-import {Text, View, StyleSheet, FlatList, CheckBox, TouchableOpacity} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import {
+  Text, View, StyleSheet, FlatList, TouchableOpacity,
+} from 'react-native';
 import * as Contacts from 'expo-contacts';
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingTop: 10,
+  },
+  paragraph: {
+    margin: 24,
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    color: '#34495e',
+  },
+  item: {
+    fontSize: 18,
+    flexDirection: 'row',
+    alignItems: 'center',
+    paddingTop: 5,
+    paddingBottom: 5,
+  },
+  itemSelected: {
+    backgroundColor: '#2F74C5',
+  },
+  itemUnselected: {
+    backgroundColor: 'white',
+  },
+  selection: {
+    height: 18,
+    width: 18,
+    margin: 5,
+    borderRadius: 18 / 2,
+    backgroundColor: '#2F74C5',
+    borderWidth: 1,
+  },
+});
 
 async function loadContacts(setContact) {
-    const permission = await Contacts.requestPermissionsAsync();
+  const permission = await Contacts.requestPermissionsAsync();
 
-    if (permission.status !== 'granted') {
-        return;
-    }
+  if (permission.status !== 'granted') {
+    return;
+  }
 
-    const contacts = await Contacts.getContactsAsync({
-        fields: [
-            Contacts.PHONE_NUMBERS,
-        ],
-        pageSize: 10000,
-        pageOffset: 0,
-    });
+  const contacts = await Contacts.getContactsAsync({
+    fields: [
+      Contacts.PHONE_NUMBERS,
+    ],
+    pageSize: 10000,
+    pageOffset: 0,
+  });
 
-    const contactsCleaned = contacts.data.filter((contact, index) => {
-        return index === contacts.data.findIndex((obj) => {
-            return obj.id === contact.id;
-        }) && contact.phoneNumbers;
-    });
+  const contactsCleaned = contacts.data.filter((contact, index) => {
+    const indexFound = contacts.data.findIndex((obj) => obj.id === contact.id);
+    return index === indexFound && contact.phoneNumbers;
+  });
 
-    setContact(contactsCleaned);
+  setContact(contactsCleaned);
+}
+
+function itemSelected(item, selected, setSelected, selectedContactChanged) {
+  const currentlySelected = { ...selected };
+
+  const { id } = item;
+
+  if (currentlySelected[id]) {
+    currentlySelected[id] = !currentlySelected[id];
+  } else {
+    currentlySelected[id] = true;
+  }
+
+  setSelected(currentlySelected);
+
+  if (selectedContactChanged) {
+    selectedContactChanged(currentlySelected);
+  }
+}
+
+function renderItem(item, selected, setSelected, selectedContactChanged) {
+  const selectionStyle = [
+    styles.item,
+    styles.selection,
+    selected[item.id] ? styles.itemSelected : styles.itemUnselected,
+  ];
+
+  const onPressHandler = () => itemSelected(item, selected, setSelected, selectedContactChanged);
+  return (
+    <TouchableOpacity style={styles.item} onPress={onPressHandler}>
+      <Text style={selectionStyle} />
+      <Text style={styles.item}>{item.name}</Text>
+    </TouchableOpacity>
+  );
 }
 
 export default function ContactsList(props) {
+  const [contacts, setContacts] = useState([]);
+  const [selected, setSelected] = useState({});
 
-    const [contacts, setContacts] = useState([]);
-    const [selected, setSelected] = useState({});
-
-    useEffect(() => {
-        async function load() {
-            await loadContacts(setContacts);
-        }
-        load();
-    }, []);
-
-    const {filter = '', selectedContactChanged = null} = props;
-
-    // let filtered = [];
-    // for (let i = 0; i < contacts.length; i++) {
-    //     const contact = contacts[i];
-    //     if (contact.name.match('(' + filter + ')\\w+'))
-    //         filtered.push(contact);
-    // }
-
-    return (
-        <View style={styles.container}>
-            <Text>{0} contacts selected.</Text>
-            <FlatList
-                data={contacts}
-                renderItem={({item}) => renderItem(item, selected, setSelected, selectedContactChanged)}
-            />
-        </View>
-    );
-}
-
-
-function renderItem(item, selected, setSelected, selectedContactChanged)
-{    
-    return (
-        <TouchableOpacity style={styles.item}
-                          onPress={() => itemSelected(item, selected, setSelected, selectedContactChanged)}>
-            <Text style={[styles.item, styles.selection, selected[item.id] ? styles.itemSelected : styles.itemUnselected]}> </Text>
-            <Text style={styles.item}>{item.name}</Text>
-        </TouchableOpacity>
-    );
-}
-
-function itemSelected(item, selected, setSelected, selectedContactChanged)
-{
-    const currentlySelected = {...selected};
-
-    const id = item.id;
-
-    if (currentlySelected[id]) {
-        currentlySelected[id] = !currentlySelected[id];
-    } else {
-        currentlySelected[id] = true;
+  useEffect(() => {
+    async function load() {
+      await loadContacts(setContacts);
     }
+    load();
+  }, []);
 
-    setSelected(currentlySelected);
+  const { filter = '', selectedContactChanged = null } = props;
 
-    if (selectedContactChanged)
-        selectedContactChanged(currentlySelected);
+  const filtered = contacts.filter((contact) => contact.name.match(`(${filter})\\w+`));
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={filtered}
+        renderItem={({ item }) => renderItem(item, selected, setSelected, selectedContactChanged)}
+      />
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        paddingTop: 10,
-    },
-    paragraph: {
-        margin: 24,
-        fontSize: 18,
-        fontWeight: 'bold',
-        textAlign: 'center',
-        color: '#34495e',
-    },
-    item: {
-        fontSize: 18,
-        flexDirection: 'row',
-        alignItems: 'center',
-        paddingTop: 5,
-        paddingBottom: 5,
-    },
-    itemSelected: {
-        backgroundColor: '#2F74C5'
-    },
-    itemUnselected: {
-        backgroundColor: 'white'
-    },
-    selection: {
-        height: 18,
-        width: 18,
-        margin: 5,
-        borderRadius: 18/2,
-        backgroundColor: '#2F74C5',
-        borderWidth: 1
-    }
-});
+ContactsList.propTypes = {
+  filter: PropTypes.string,
+  selectedContactChanged: PropTypes.func,
+};
+
+ContactsList.defaultProps = {
+  filter: '',
+  selectedContactChanged: null,
+};
